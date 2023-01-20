e5baf595c7b44a788eb41499db634071db1341c3
Andy Grove <andygrove73@gmail.com>
2023-01-20 09:51:47 -0700

    clippy and fmt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,4 @@
+use qpml::from_text_plan;
 use std::fs;
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -24,24 +25,34 @@ enum Opt {
         #[structopt(long)]
         inverted: bool,
     },
+    /// Import a text plan and convert to QPML
+    ImportText {
+        #[structopt(parse(from_os_str))]
+        input: PathBuf,
+    },
 }
 
 fn main() {
     match Opt::from_args() {
         Opt::Print { input } => {
-            let yaml = fs::read_to_string(&input).expect("Unable to read file");
+            let yaml = fs::read_to_string(input).expect("Unable to read file");
             let doc: qpml::Document = serde_yaml::from_str(&yaml).unwrap();
             qpml::generate_text(&doc);
         }
         Opt::Dot { input, inverted } => {
-            let yaml = fs::read_to_string(&input).expect("Unable to read file");
+            let yaml = fs::read_to_string(input).expect("Unable to read file");
             let doc: qpml::Document = serde_yaml::from_str(&yaml).unwrap();
             qpml::generate_dot(&doc, inverted);
         }
         Opt::Mermaid { input, inverted } => {
-            let yaml = fs::read_to_string(&input).expect("Unable to read file");
+            let yaml = fs::read_to_string(input).expect("Unable to read file");
             let doc: qpml::Document = serde_yaml::from_str(&yaml).unwrap();
             qpml::generate_mermaid(&doc, inverted);
+        }
+        Opt::ImportText { input } => {
+            let doc = from_text_plan(&input).unwrap();
+            let str = serde_yaml::to_string(&doc).unwrap();
+            println!("{}", str);
         }
     }
 }
